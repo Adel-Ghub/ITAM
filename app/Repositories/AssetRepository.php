e48c2182141fb72c3fc11cc76e769cc6e7b98a3d
AdelN94 <adel.n@totersapp.com>
2023-01-27 15:45:48 +0200

    first commit

---
+++ app/Repositories/AssetRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Asset;
+
+class AssetRepository
+{
+    protected $asset;
+
+    public function __construct(Asset $asset)
+    {
+        $this->asset = $asset;
+    }
+
+    public function getAll()
+    {
+        return $this->asset->all();
+    }
+
+    public function create($attributes)
+    {
+        return $this->asset->create($attributes);
+    }
+
+    public function update($id, array $attributes)
+    {
+        return $this->asset->find($id)->update($attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->asset->find($id)->delete();
+    }
+}
