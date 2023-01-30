2fbc9a1bb2d94a6ed18cc881034f40695d77364b
AdelN94 <adel.n@totersapp.com>
2023-01-30 11:50:20 +0200

    Setting up assets

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Repositories\AssetRepository;
 
 use Illuminate\Http\Request;
 
 class AssetController extends Controller
 {
-    //
+    protected $assetRepository;
+
+    public function __construct(AssetRepository $assetRepository)
+    {
+        $this->assetRepository = $assetRepository;
+    }
+
+    public function index()
+    {
+        $assets = $this->assetRepository->all();
+        return view('assets.index', compact('assets'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->assetRepository->create($request->all());
+        return redirect()->route('assets.index')->with('success', 'Asset added successfully');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->assetRepository->update($id, $request->all());
+        return redirect()->route('assets.index')->with('success', 'Asset updated successfully');
+    }
+
+    public function destroy($id)
+    {
+        $this->assetRepository->delete($id);
+        return redirect()->route('assets.index')->with('success', 'Asset deleted successfully');
+    }
 }
